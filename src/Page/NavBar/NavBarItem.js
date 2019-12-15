ece351628a2158b50065583fcc63c2ec72b33b95
szczupag <jadziutka@gmail.com>
2019-12-15 15:02:07 +0100

    add navbar item icons

--- src/Page/NavBar/NavBarItem.js
+++ src/Page/NavBar/NavBarItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
 
-const NavBarItem = ({ name, location }) => {
+const NavBarItem = ({ name, location, icon }) => {
     const path = `/${name}`
     return (
         <li className={location.pathname === path ? "active" : ""}>
-            <NavLink to={path}>{name}</NavLink>
+            <NavLink to={path}>
+                <img src={icon} />
+                {name}
+            </NavLink>
         </li>
     )
 }
--- src/Page/NavBar/index.js
+++ src/Page/NavBar/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavBarItem from './NavBarItem'
+import nap from '../../assets/nap.svg'
+import sleep from '../../assets/sleep.svg'
+import plan from '../../assets/plan.svg'
+import report from '../../assets/report.svg'
 
 const NavBar = () => (
     <div className="navbar-wrapper">
         <ul className="navbar mobile-container">
-            <NavBarItem name="sleep" />
-            <NavBarItem name="report" />
-            <NavBarItem name="nap" />
-            <NavBarItem name="plan" />
+            <NavBarItem name="sleep" icon={sleep} />
+            <NavBarItem name="report" icon={report} />
+            <NavBarItem name="nap" icon={nap} />
+            <NavBarItem name="plan" icon={plan} />
         </ul>
     </div>
 )
