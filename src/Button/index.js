0d06c528c596f5ec3aeedb606ec85dedb5c170b3
szczupag <jadziutka@gmail.com>
2019-12-17 18:45:18 +0100

    add page components

---
+++ src/Button/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({ children }) => (
+    <div className="btn-wrapper">
+        <button>{children}</button>
+    </div>
+)
+
+export default Button
--- src/Page/Sleep.js
+++ src/Page/Sleep.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PageWrapper from './PageWrapper'
+import Button from '../Button'
 
 const Sleep = () => (
     <PageWrapper>
-        Sleep
+        <h2>Set sleep hours</h2>
+        <h2>Wake up shift</h2>
+        <Button>Sleep</Button>
     </PageWrapper>
 )
 
