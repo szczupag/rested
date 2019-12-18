bb9a29db9d10ca901d30906952ca0d3bdf82eb7e
szczupag <jadziutka@gmail.com>
2019-12-18 21:10:06 +0100

    create Nap component & refactor Start view & add sleep rating

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,11 @@ import Sleep from './Page/Sleep'
 import Nap from './Page/Nap'
 import Plan from './Page/Plan'
 import Report from './Page/Report'
-import SleepStart from './Page/SleepStart'
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/sleep" component={Sleep} />
-      <Route path="/start" component={SleepStart} />
       <Route path="/report" component={Report} />
       <Route path="/nap" component={Nap} />
       <Route path="/plan" component={Plan} />
