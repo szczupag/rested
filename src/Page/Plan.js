0ff184e552a56e46d1939edb535ada1633c0758d
szczupag <jadziutka@gmail.com>
2019-12-18 21:43:04 +0100

    improve styles

--- src/Page/Plan.js
+++ src/Page/Plan.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import PageWrapper from './PageWrapper'
-import {Line} from 'rc-progress';
+import { Line } from 'rc-progress';
 import plan1 from '../assets/plan1_1.svg'
 import plan2 from '../assets/plan2_2.svg'
 import plan3 from '../assets/plan3.svg'
-import SleepEnded from "./SleepEnded";
 
 const data = [{
     title: "Sleepyhead",
@@ -32,17 +30,18 @@ const Plan = () => (
         {
             data.map((d) =>
                 <div className="goal-tile-wrapper">
-                    <h1>{d.title}</h1>
-                    <h2>{d.current_progress}/{d.goal}</h2>
+                    <div className="goal-tile-header">
+                        <h1>{d.title}</h1>
+                        <h2>{d.current_progress}/{d.goal}</h2>
+                    </div>
                     <p>{d.description}</p>
-                    <img src={d.img} className="tile-icon"/>
+                    <img src={d.img} className="tile-icon" />
                     <div className="goal-progressbar-wrapper">
-                        <Line percent={d.current_progress / d.goal * 100} strokeWidth="1" strokeColor="#86FFEA"/>
+                        <Line percent={d.current_progress / d.goal * 100} strokeWidth="1" strokeColor="#86FFEA" trailColor="#8d9fab" />
                     </div>
                 </div>
             )
         }
-        <SleepEnded n={1}/>
     </>
 )
 
